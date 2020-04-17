d368612c542c99b5aab711fe88b481853351078b
Дмитрий Козынцев <falcon-x005.93@list.ru>
2020-04-17 20:50:22 +0500

    Добавлен проект для тестов

---
+++ UnitTests/CommonTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CommonTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testVar = 2 + 2;
+            Assert.AreEqual(4, testVar);
+        }
+    }
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("95d66176-113f-4fe0-bb23-3b9e81e0ffa8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
